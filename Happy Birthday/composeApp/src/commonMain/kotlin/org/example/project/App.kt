c319110bea6074f4fc10418bf1d1bba82365b3b4
carlospadron <padron.ca@gmail.com>
2024-10-19 07:12:06 +0100

    adding images

--- Happy Birthday/composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ Happy Birthday/composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -3,12 +3,15 @@ package org.example.project
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -19,7 +22,9 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.dp
 import happybirthday.composeapp.generated.resources.Res
+import happybirthday.composeapp.generated.resources.androidparty
 import happybirthday.composeapp.generated.resources.compose_multiplatform
+import androidx.compose.ui.layout.ContentScale
 
 @Composable
 @Preview
@@ -40,33 +45,52 @@ fun App() {
 //        }
 //    }
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            Modifier.fillMaxWidth().padding(8.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+        Surface(
+            modifier = Modifier.fillMaxSize()
         ) {
-            GreetingText(
-                "Happy Birthday Sam!",
-                "From: Compose"
-            )
+            Box(Modifier) {
+                GreetingImage()
+                GreetingText(
+                    "Happy Birthday Sam!",
+                    "From: Compose"
+                )
+
+            }
         }
     }
 }
 
 @Composable
-fun GreetingText(message: String, from: String) {
-    Text(
-        message,
-        fontSize = 100.sp,
-        lineHeight = 116.sp,
-        textAlign = TextAlign.Center
-    )
-    Text(
-        text = from,
-        fontSize = 36.sp,
-        modifier = Modifier
-            .padding(16.dp),
-        textAlign = TextAlign.End
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(top = 16.dp)
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .padding(end = 16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
+}
+@Composable
+fun GreetingImage(modifier: Modifier = Modifier) {
+    val image = painterResource(Res.drawable.androidparty)
+    Image(
+        painter = image,
+        contentDescription = null,
+        modifier = modifier,
+        contentScale = ContentScale.Crop,
+        alpha = 0.5F
     )
 }
